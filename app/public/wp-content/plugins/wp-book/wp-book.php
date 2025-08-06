a510f9f08a993b5968ba52f52fe1b7598b4a90b6
vinay <tanish.chavada@gmail.com>
2025-08-07 00:02:32 +0530

    Add non-hierarchical taxonomy 'Book Tag' for books

--- app/public/wp-content/plugins/wp-book/wp-book.php
+++ app/public/wp-content/plugins/wp-book/wp-book.php
@@ -81,3 +81,37 @@ function wp_book_register_taxonomy_category() {
 }
 add_action( 'init', 'wp_book_register_taxonomy_category' );
 
+
+
+// Register Non-Hierarchical Taxonomy: Book Tag
+function wp_book_register_taxonomy_tag() {
+    $labels = array(
+        'name'                       => __( 'Book Tags', 'wp-book' ),
+        'singular_name'              => __( 'Book Tag', 'wp-book' ),
+        'search_items'               => __( 'Search Book Tags', 'wp-book' ),
+        'popular_items'              => __( 'Popular Book Tags', 'wp-book' ),
+        'all_items'                  => __( 'All Book Tags', 'wp-book' ),
+        'edit_item'                  => __( 'Edit Tag', 'wp-book' ),
+        'update_item'                => __( 'Update Tag', 'wp-book' ),
+        'add_new_item'               => __( 'Add New Tag', 'wp-book' ),
+        'new_item_name'              => __( 'New Tag Name', 'wp-book' ),
+        'separate_items_with_commas' => __( 'Separate tags with commas', 'wp-book' ),
+        'add_or_remove_items'        => __( 'Add or remove tags', 'wp-book' ),
+        'choose_from_most_used'      => __( 'Choose from the most used tags', 'wp-book' ),
+        'menu_name'                  => __( 'Book Tags', 'wp-book' ),
+    );
+
+    $args = array(
+        'hierarchical'          => false, // NON-HIERARCHICAL = like tags
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_in_rest'          => true,
+        'show_admin_column'     => true,
+        'update_count_callback' => '_update_post_term_count',
+        'rewrite'               => array( 'slug' => 'book-tag' ),
+    );
+
+    register_taxonomy( 'book_tag', 'book', $args );
+}
+add_action( 'init', 'wp_book_register_taxonomy_tag' );
+
